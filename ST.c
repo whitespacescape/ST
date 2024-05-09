a1509506a1022107d7dbba8579295dd11317d0f6
spacescape <15368104568@189.cn>
2024-05-09 03:05:19 -0700

    Add files via upload

---
+++ ST.c
@@ -0,0 +1,14 @@
+#include "stdarg.h"
+#include "stdlib.h"
+#include "stddef.h"
+#include "string.h"
+#include "stdio.h"
+void S(){
+    typedef s.tdarg
+    typedef s.tdlib
+    typedef s.tddef
+}
+void T(){
+    typeof st.ring
+    typeof st.dio
+}
