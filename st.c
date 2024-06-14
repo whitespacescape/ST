d05e1f23debd2b7cba35e3e6519cf2e56ddddeee
ace <15368104568@189.cn>
2024-06-14 20:16:10 +0800

    Create st.c

---
+++ st.c
@@ -0,0 +1,15 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+#define A Area
+#define T Temperature
+#define k 0.00000 00000 00000 00000 001 380649
+#define c 299792458
+#define H 0.00000 00000 00000 00000 00000 00000 0001 054571817
+#define G 0.00000 00000 066743
+void ST(){
+    int S = [(A*k*c*c*c)/(4*H*G)]
+    int T = [(H*c*c)/(8*H*k*G*M)]
+}
