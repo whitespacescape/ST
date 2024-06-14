a7f69add23d0e71234f03cbc2c207073b09b9562
ace <15368104568@189.cn>
2024-06-15 05:34:17 +0800

    Update st.c

--- st.c
+++ st.c
@@ -5,10 +5,10 @@
 #include <stdio.h>
 #define A Area
 #define T Temperature
-#define k 0.00000 00000 00000 00000 001 380649
 #define c 299792458
-#define H 0.00000 00000 00000 00000 00000 00000 0001 054571817
 #define G 0.00000 00000 066743
+#define k 0.00000 00000 00000 00000 001 380649
+#define H 0.00000 00000 00000 00000 00000 00000 0001 054571817
 void ST(){
     int S = [(A*k*c*c*c)/(4*H*G)]
     int T = [(H*c*c)/(8*H*k*G*M)]
